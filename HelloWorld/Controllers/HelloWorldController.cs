e1069ff6c76fb5113e6f875eacfe932d6fac7856
Kamlekar <kamlekar.venkatesh@gmail.com>
2019-01-22 17:25:40 +0530

    new changes from https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/start-mvc?view=aspnetcore-2.2&tabs=visual-studio

---
+++ HelloWorld/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace HelloWorld.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int numTimes = 1) {
+            return HtmlEncoder.Default.Encode($"Hello {name}, No. of Visits is: {numTimes}");
+        }
+    }
+}
--- MVCRazorDemo/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCRazorDemo.MvcApplication" Language="C#" %>
